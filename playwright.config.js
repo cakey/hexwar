c159ed6e207bc913f72d48f3a8571c79b364a881
Ben Shaw <benshaw9@gmail.com>
2026-07-20 22:04:41 -0700

    Allow software WebGL time in CI

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,10 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './e2e',
+  timeout: process.env.CI ? 90_000 : 30_000,
+  expect: {
+    timeout: process.env.CI ? 15_000 : 5_000,
+  },
   fullyParallel: true,
   forbidOnly: Boolean(process.env.CI),
   retries: process.env.CI ? 2 : 0,
